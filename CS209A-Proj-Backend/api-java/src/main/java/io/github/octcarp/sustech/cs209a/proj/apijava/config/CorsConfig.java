5cad06df656ccdeb105fa678455bd05ab05fd3e8
OctCarp <octcarp@hotmail.com>
2024-12-18 12:00:37 +0800

    feat: Update CORS for server

---
+++ CS209A-Proj-Backend/api-java/src/main/java/io/github/octcarp/sustech/cs209a/proj/apijava/config/CorsConfig.java
@@ -0,0 +1,17 @@
+package io.github.octcarp.sustech.cs209a.proj.apijava.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:8081", "http://localhost:20249")
+            .allowedMethods("GET", "POST", "PUT", "DELETE")
+            .allowCredentials(true)
+            .maxAge(3600);
+    }
+}
---
+++ CS209A-Proj-Backend/sql/database.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE cs209a_proj
+    WITH
+    OWNER = cs209a
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'en_US.utf8'
+    LC_CTYPE = 'en_US.utf8';
+
+GRANT ALL PRIVILEGES ON DATABASE cs209a_proj TO cs209a;
