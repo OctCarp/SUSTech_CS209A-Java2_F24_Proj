db8e1d276a0d6e4a2e710300903b71a6e5d5ff33
OctCarp <octcarp@hotmail.com>
2024-12-26 15:01:54 +0800

    Remove useless multi-pages dependency

--- CS209A-Proj-Frontend/src/main.js
+++ CS209A-Proj-Frontend/src/main.js
@@ -1,9 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
 import App from './App.vue'
-import router from './router'
 import 'vuetify/styles';
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
@@ -16,6 +14,5 @@ const vuetify = createVuetify({
     directives,
 })
 
-app.use(createPinia())
 app.use(vuetify)
 app.mount('#app')
